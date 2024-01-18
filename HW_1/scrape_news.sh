cec1af11f698f77ba7670bb66cc8898a8ebb5800
sari <sarimansour123@gmail.com>
2024-01-18 18:08:27 +0200

    finished

--- HW_1/scrape_news.sh
+++ HW_1/scrape_news.sh
@@ -5,18 +5,19 @@ URLs=$(grep -oP "https://www.ynetnews.com/article/[a-zA-Z0-9]+" 3082 |
 	   sort |
 	   uniq)
 
-echo $URLs | wc -w > results.csv
-wget -iO - $URLs
-for line in $URLs ;
+echo "$URLs" | wc -w > results.csv
+wget -iO - "$URLs"
+while read -r line;
   do
 	article=$(echo "$line" | grep -o '[^/]\+$')
 
 	N=$(grep -o Netanyahu "$article" | wc -l)
 	G=$(grep -o Gantz "$article" | wc -l)
-    if (( (( $N==0 )) && (( $G==0 )) )); then
-		echo "$line"", -" >> results.csv
-	else 
-	    echo "$line"", Netanyahu,"" $N"", Gantz,"
-	    " $G" >> results.csv
+
+  if ((N==0)) && ((G==0)); then
+		  echo "$line"", -" >> results.csv
+	else
+	    echo "$line"", Netanyahu,"" $N"", Gantz,"" $G" >> results.csv
 	fi
-done
+done <<< "$URLs"
+
