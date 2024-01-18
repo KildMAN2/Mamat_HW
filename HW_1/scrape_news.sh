d75e9f0219041964b890c4d2c1fb3986fbe33182
sari mansour <130569051+KildMAN2@users.noreply.github.com>
2024-01-18 12:42:36 +0200

    Add files via upload
    finished

---
+++ HW_1/scrape_news.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+wget https://www.ynetnews.com/category/3082 
+URLs=$(grep -oP "https://www.ynetnews.com/article/[a-zA-Z0-9]+" 3082 | 
+	   sort |
+	   uniq)
+	   
+echo $URLs | wc -w > results.csv
+wget -iO - $URLs
+for line in $URLs ;
+  do
+	article=$(echo "$line" | grep -o '[^/]\+$') 
+	
+	N=$(grep -o Netanyahu "$article" | wc -l)
+	G=$(grep -o Gantz "$article" | wc -l)
+	B=$(grep -o Bennett "$article" | wc -l)
+	P=$(grep -o Peretz "$article" | wc -l)
+	
+    if (( (( $N==0 )) && (( $G==0 )) && (( $B==0 )) && (( $P==0 )) )); then
+		echo "$line"", -" >> results.csv
+	else 
+	    echo "$line"", Netanyahu,"" $N"", Gantz,"
+	    " $G"", Bennett,"" $B"", Perez,""$P" >> results.csv
+	fi
+done
+ 
