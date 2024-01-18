d35ce96c7904e5a2ee2adc2e9c38bf6e52c44633
sari <sarimansour123@gmail.com>
2024-01-18 14:53:11 +0200

    finished

--- HW_1/scrape_news.sh
+++ HW_1/scrape_news.sh
@@ -6,20 +6,19 @@ URLS=$(grep -oP "https://www.ynetnews.com/article/[a-zA-Z0-9]+" 3082 |
 	   uniq)
 
 echo "$URLS" | wc -w > results.csv
-
 for url in $URLS ;
   do
-    wqet -q "$url" -O article.html
-	article_name=$(echo "$url" | awk -F/ '{print $NF}')
+  wqet -q "$url" -o article.html
+	article_name=$(echo "$url" | grep -o "https://www.ynetnews.com/article/\K[a-zA-Z0-9]+")
 
 	N=$(grep -o 'Netanyahu' article.html | wc -l)
 	G=$(grep -o 'Gantz' article.html | wc -l)
   total_count=$((N + G))
 
   if (( total_count == 0 )); then
-		  echo "$url, -" >> results.csv
+		  echo "$url, $article_name, -" >> results.csv
 	else
-	    echo "$url, Netanyahu, $N, Gantz, $G" >> results.csv
+	    echo "$url, $article_name, Netanyahu, $N, Gantz, $G" >> results.csv
 	fi
 done
 
