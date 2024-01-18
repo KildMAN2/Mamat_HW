79bce80c1c22da79c5381bdc8fcba31c86bb106c
sari <sarimansour123@gmail.com>
2024-01-18 23:33:15 +0200

    finished

--- HW_1/scrape_news.sh
+++ HW_1/scrape_news.sh
@@ -1,23 +1,28 @@
-#! /bin/bash
+#!/bin/bash
 
-wget https://www.ynetnews.com/category/3082
-URLs=$(grep -oP "https://www.ynetnews.com/article/[a-zA-Z0-9]+" 3082 |
-	   sort |
-	   uniq)
+# שלב ראשון: הורדת עמוד החדשות הראשי
+wget https://www.ynetnews.com/category/3082 -O main_page.html
 
-echo "$URLs" | wc -w > results.csv
-wget -iO - "$URLs"
-while read -r line;
-  do
-	article=$(echo "$line" | grep -o '[^/]\+$')
+# שלב שני: חיפוש ושמירת קישורים לכתבות
+grep -Eo "https://www\.ynetnews\.com/article/[a-zA-Z0-9]+" main_page.html > article_links.txt
 
-	N=$(grep -o Netanyahu "$article" | wc -l)
-	G=$(grep -o Gantz "$article" | wc -l)
+# שלב שלישי: ספירת מספר הקישורים
+num_articles=$(wc -l < article_links.txt)
+echo "Number of articles: $num_articles"
 
-  if ((N==0)) && ((G==0)); then
-		  echo "$line"", -" >> results.csv
-	else
-	    echo "$line"", Netanyahu,"" $N"", Gantz,"" $G" >> results.csv
-	fi
-done <<< "$URLs"
+# שלב רביעי: לעבור על קישורים ולבדוק מספר הפעמים שמופיעים שמות משפחה
+while IFS= read -r link; do
+    # הורדת הכתבה
+    wget "$link" -O article.html
 
+    # ספירת מספר הופעות שמות משפחה
+    netanyahu_count=$(grep -io "Netanyahu" article.html | wc -l)
+    gantz_count=$(grep -io "Gantz" article.html | wc -l)
+
+    # הדפסת התוצאה
+    echo "$link, Netanyahu: $netanyahu_count, Gantz: $gantz_count"
+
+    # מחיקת הקובץ של הכתבה
+    rm article.html
+
+done < article_links.txt
