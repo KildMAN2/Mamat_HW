ee6061246c4574b18348d7541f533f3a17dca718
sari <sarimansour123@gmail.com>
2024-01-21 18:49:00 +0200

    finished

--- HW_1/scrape_news.sh
+++ HW_1/scrape_news.sh
@@ -1,23 +1,24 @@
 #! /bin/bash
 
-wget https://www.ynetnews.com/category/3082 -O main_page.html
-URLs=$(grep -oP "https://www.ynetnews.com/article/[a-zA-Z0-9]+" main_page.html|
+site=https://www.ynetnews.com/category/3082
+web_data=$(wget --no-check-certificate -o- "$site" 2>/dev/null)
+URLs=$(echo "$web_data" |\ grep -oP "https://(www.)?ynetnews.com/article/[a-zA-Z0-9]+"
 	   sort |
 	   uniq)
 
-echo "Number of articles: $(echo "$URLs" | wc -w )"> results.csv
-while read -r link;
+wc -l <<< "$URLs" >> results.csv
+ for url in $URLs;
   do
-  wget "$link" -O article.html
-	N=$(grep -o "Netanyahu" article.html | wc -l)
-	G=$(grep -o "Gantz" article.html | wc -l)
+	echo -n "$url" >> results.csv
+	article=$(wget --no-check-certificate -o- "$url" 2>/dev/null)
 
-  if ((N==0)) && ((G==0)); then
-		  echo "$link, -" >> results.csv
+	N=$(grep -o "Netanyahu" <<< "$article" | wc -w)
+	G=$(grep -o "Gantz" <<< "$article" | wc -w)
+
+  if [ "$N" -eq 0 ] && [ "$G" -eq 0 ]; then
+		  echo ", -" >> results.csv
 	else
-	    echo "$link, Netanyahu, $N, Gantz, $G" >> results.csv
+	    echo ", Netanyahu,"" $N"", Gantz,"" $G" >> results.csv
 	fi
-	rm article.html
-done <<< "$URLs"
-
+done
 
