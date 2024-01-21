fb1ec457c79b33f0214c7ad9fa7a58594bc9756a
sari <sarimansour123@gmail.com>
2024-01-21 20:21:00 +0200

    finished

--- HW_1/scrape_news.sh
+++ HW_1/scrape_news.sh
@@ -5,7 +5,7 @@ URLs=$(grep -oP "https://www.ynetnews.com/article/[a-zA-Z0-9]+" 3082 |
 	   sort |
 	   uniq)
 	   
-wget -iO - $URLs
+wget -iO - "$URLs"
 for line in $URLs ;
   do
 	article=$(echo "$line" | grep -o '[^/]\+$') 
@@ -14,7 +14,7 @@ for line in $URLs ;
 	G=$(grep -o Gantz "$article" | wc -l)
 
 	
-    if (( (( $N==0 )) && (( $G==0 )))); then
+    if (( (( "$N"==0 )) && (( "$G"==0 )))); then
 		echo "$line"", -"
 	else 
 	    echo "$line"", Netanyahu,"" $N"", Gantz,"
