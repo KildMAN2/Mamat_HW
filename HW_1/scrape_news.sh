d08c8acb204aeea4c8b425d630366e906d66fbe7
sari <sarimansour123@gmail.com>
2024-01-21 19:01:49 +0200

    finished

--- HW_1/scrape_news.sh
+++ HW_1/scrape_news.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 
-site=https://www.ynetnews.com/category/3082
+site="https://www.ynetnews.com/category/3082"
 web_data=$(wget --no-check-certificate -O- "$site" 2>/dev/null)
-URLs=$(echo "$web_data" |\ grep -oP "https://(www.)?ynetnews.com/article/[a-zA-Z0-9]+"
-	   sort |
-	   uniq)
-
+URLs=$(echo "$web_data" | grep -oP "https://(www.)?ynetnews.com/article/[a-zA-Z0-9]+" | sort | uniq)
 wc -l <<< "$URLs" >> results.csv
  for url in $URLs;
   do
@@ -15,9 +12,9 @@ wc -l <<< "$URLs" >> results.csv
 	G=$(grep -o "Gantz" <<< "$article" | wc -w)
 
   if [ "$N" -eq 0 ] && [ "$G" -eq 0 ]; then
-		  echo "$url, -" >> results.csv
+    echo "$url, -" >> results.csv
 	else
-	    echo "$url, Netanyahu,"" $N"", Gantz,"" $G" >> results.csv
+    echo "$url, Netanyahu, $N, Gantz, $G" >> results.csv
 	fi
 done
 
