caa4d92157260c0147cce72474a11a9bd9c2cf45
sari <sarimansour123@gmail.com>
2024-01-21 22:59:54 +0200

    finished

--- HW_1/scrape_news.sh
+++ HW_1/scrape_news.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
+#! /bin/bash
 
 site="https://www.ynetnews.com/category/3082"
 web_data=$(wget --no-check-certificate -O- "$site" 2>/dev/null)
-URLs=$(echo "$web_data" | grep -oP "https://(www.)?ynetnews.com/article/[a-zA-Z0-9]+" | uniq | sort)
-
+URLs=$(echo "$web_data" | grep -oP "https://(www.)?ynetnews.com/article/[a-zA-Z0-9]+" | sort | uniq)
  for url in $URLs;
   do
 	article=$(wget --no-check-certificate -O- "$url" 2>/dev/null)
 
-	N=$(grep -oP "Netanyahu" <<< "$article" | wc -l)
-	G=$(grep -oP "Gantz" <<< "$article" | wc -l)
+	N=$(grep -o "Netanyahu" <<< "$article" | wc -w)
+	G=$(grep -o "Gantz" <<< "$article" | wc -w)
 
   if [ "$N" -eq 0 ] && [ "$G" -eq 0 ]; then
-    echo "$url,"""-
+    echo "$url, -"
 	else
-    echo "$url,""Netanyahu, $N,""Gantz, ""$G"
+    echo "$url, Netanyahu, $N, Gantz, $G"
 	fi
 done
