6048d34c91d32aca98b04ad6a15a5f078cda60f4
sari <sarimansour123@gmail.com>
2024-01-21 19:34:09 +0200

    finished

--- HW_1/scrape_news.sh
+++ HW_1/scrape_news.sh
@@ -1,24 +1,20 @@
+User
 #! /bin/bash
 
-wget https://www.ynetnews.com/category/3082
-URLs=$(grep -oP "https://www.ynetnews.com/article/[a-zA-Z0-9]+" 3082 |
-	   sort |
-	   uniq)
-
-echo $URLs | wc -w
-wget -iO - $URLs
-for line in $URLs ;
+site="https://www.ynetnews.com/category/3082"
+web_data=$(wget --no-check-certificate -O- "$site" 2>/dev/null)
+URLs=$(echo "$web_data" | grep -oP "https://(www.)?ynetnews.com/article/[a-zA-Z0-9]+" | sort | uniq)
+wc -l <<< "$URLs"
+ for url in $URLs;
   do
-	article=$(echo "$line" | grep -o '[^/]\+$')
+	article=$(wget --no-check-certificate -O- "$url" 2>/dev/null)
 
-	N=$(grep -o Netanyahu "$article" | wc -l)
-	G=$(grep -o Gantz "$article" | wc -l)
+	N=$(grep -o "Netanyahu" <<< "$article" | wc -w)
+	G=$(grep -o "Gantz" <<< "$article" | wc -w)
 
-
-    if (( (( $N==0 )) && (( $G==0 )) )); then
-		echo "$line"", -"
+  if [ "$N" -eq 0 ] && [ "$G" -eq 0 ]; then
+    echo "$url, -" 
 	else
-	    echo "$line"", Netanyahu,"" $N"", Gantz,"
-	    " $G"
+    echo "$url, Netanyahu, $N, Gantz, $G"
 	fi
 done
